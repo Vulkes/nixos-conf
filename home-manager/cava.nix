78f31521a21b8099b58790ef096ef8f98bea3678
Signor-Koala <signor-koala@signor-koala.dev>
2026-01-14 01:47:26 +0530

    Change cava to nocurses

--- home-manager/cava.nix
+++ home-manager/cava.nix
@@ -5,14 +5,12 @@
 }: {
   programs.cava = {
     enable = true;
-    package = pkgs.cava.override {withSDL2 = true;};
     settings = {
       general = {
         max_height = 80;
         sleep_timer = 30;
       };
       output = {
-        method = "sdl_glsl";
         channels = "mono";
         mono_option = "average";
         synchronized_sync = 1;
